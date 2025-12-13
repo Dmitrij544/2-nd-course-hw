b610dd94b982428b22583322f2e0d470f0080e0f
Dmitrij <dmitrijsarycev31@gmail.com>
2025-12-14 02:53:41 +0500

    Изменения

---
+++ script.js
@@ -0,0 +1,57 @@
+/*/*Задание №1*/
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+/*Задание №2*/
+let firstIphoneYear = 2007;
+alert(firstIphoneYear);
+
+/*Задание №3*/
+let creatorName = "Brendan Eich";
+alert(creatorName);
+
+/*Задание №4*/
+let nam1 = 10;
+let nam2 = 2;
+alert(nam1 + nam2);
+alert(nam1 - nam2);
+alert(nam1 * nam2);
+alert(nam1 / nam2);
+
+/*Задание №5*/
+let result = 2 ** 5; 
+alert(result);
+            
+/*Задание №6*/
+let a1 = 9;
+let b = 2;
+let remainder = a1 % b;
+alert(remainder);
+            
+/*Задание №7*/
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7; 
+num /= 3;
+num++;    
+num--;   
+alert(num); 
+
+/*Задание №8*/
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+/*Задание №9*/
+let user = {
+name: "Иван",
+age: 30,
+isAdmin: true 
+};
+
+/*Задание №10*/
+let userName = prompt("Как вас зовут?");
+let greetingMessage = `Привет, ${userName}!`;
+alert(greetingMessage);
