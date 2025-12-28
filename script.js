e6a4d7cdb2f59cd62228f8e064c80cb22394757f
Dmitrij <dmitrijsarycev31@gmail.com>
2025-12-28 16:51:42 +0500

    Форматирование кода 28 декабря 2025

--- script.js
+++ script.js
@@ -1,63 +1,37 @@
 /*Задание №1*/
-let password = 'пароль';
-let userGuess = prompt('Введите пароль');
-
-if (userGuess === password) {
-    alert('Пароль введен верно');
-} else {
-    alert('Пароль введен неверно');
-}
-
+console.log("Привет");
+console.log("Привет");
 /*Задание №2*/
-let c = 0;
-
-if (c > 0 && c < 10) {
-    alert('Верно'); 
-} else {
-    alert('Неверно'); 
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
 }
-
 /*Задание №3*/
-let d = 150;
-let e = 50;  
+for (let i = 7; i <= 22; i++) {
+  console.log(i);
+}
+/*Задание №4*/
+const obj = {
+  "Коля": '200',
+  "Вася": '300',
+  "Петя": '400'
+};
+/*Задание №5*/
+for (let name in obj) {
+  console.log(`${name} — зарплата ${obj[name]} долларов`);
+}
+let n = 1000;
+let num = 0;
 
-if (d > 100 || e > 100) {
-    alert('Верно');
-} else {
-    alert('Неверно');
+while (n >= 50) {
+  n = n / 2;
+  num++;
 }
 
-/*Задание №4*/
-let a = '2';
-let b = '3';
-alert(+a + +b);
+console.log("Результат:", n);
+console.log("Количество итераций:", num);
+/*Задание №6*/
+let firstFriday = 3; 
 
-/*Задание №5*/
-let monthNumber = Number(prompt('Введите номер месяца (от 1 до 12):'));
-
-if (monthNumber > 12 || monthNumber < 1) {
-    alert('Ошибка: в году всего 12 месяцев.');
-} else {
-    switch (monthNumber) {
-        case 12:
-        case 1:
-        case 2:
-            alert('Зима');
-            break;
-        case 3:
-        case 4:
-        case 5:
-            alert('Весна');
-            break;
-        case 6:
-        case 7:
-        case 8:
-            alert('Лето');
-            break;
-        case 9:
-        case 10:
-        case 11:
-            alert('Осень');
-            break;
-    }
+for (let day = firstFriday; day <= 31; day += 7) {
+    console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
 }
